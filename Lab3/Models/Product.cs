67dfca3d43215a55bc7d70128179f6aadb59b2a4
Qumay69 <145102278+Qumay69@users.noreply.github.com>
2024-12-26 14:29:03 +0200

    Update Product.cs

--- Lab3/Models/Product.cs
+++ Lab3/Models/Product.cs
@@ -6,7 +6,7 @@ namespace Lab3.Models
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-        public required decimal Price { get; set; } // Цена за единицу
+        public required decimal Price { get; set; }
     }
 
 }
